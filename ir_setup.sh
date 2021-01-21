fa2d5772b4377a4762929ab3728ca15eadb16541
chetan singh rathore <chetanrgithub@gmail.com>
2021-01-21 11:02:03 +0530

    Update ir_setup.sh

--- ir_setup.sh
+++ ir_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-git submodule update --init --recursive
+git submodule update --init
+git fetch
+git branch ir_branch
 git clone https://github.com/tianocore/edk2-libc
 cd edk2-libc
 git checkout 61687168fe02ac4d933a36c9145fdd242ac424d1
