60fa79d719ae9ecee168b5371f6d40b047e2306d
chetan singh rathore <chetanrgithub@gmail.com>
2021-01-21 15:09:51 +0530

    Update build_sbsa.sh

--- build_sbsa.sh
+++ build_sbsa.sh
@@ -3,6 +3,7 @@
 unset WORKSPACE
 unset PACKAGES_PATH
 export GCC49_AARCH64_PREFIX=/home/cherat01/ATEG/tools/gcc-linaro-5.3-2016.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export PACKAGES_PATH=$(pwd)/edk2-libc
 source edksetup.sh --reconfig
 make -C BaseTools/Source/C
-build -a AARCH64 -t GCC49 -p ShellPkg/ShellPkg.dsc -m ShellPkg/Applications/sbsa-acs/uefi_app/SbsaAvs.inf
+build -a AARCH64 -t GCC49 -p ShellPkg/ShellPkg.dsc -m ShellPkg/Application/sbsa-acs/uefi_app/SbsaAvs.inf
